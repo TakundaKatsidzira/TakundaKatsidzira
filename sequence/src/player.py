373a8c3a55bdada83ce70236caa7aa4ba254db8e
TakundaKatsidzira <katsidzirast@gmail.com>
2025-05-20 13:43:10 -0400

    Delete sequence directory

--- sequence/src/player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, player_id, name, is_cpu=False):
-        self.player_id = player_id
-        self.name = name
-        self.is_cpu = is_cpu
-        self.tokens = 0  # Track tokens placed or other stats as needed
-
-    def __str__(self):
-        return f"Player {self.name} (ID: {self.player_id})"
